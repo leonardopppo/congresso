02f60346d1ff882583e4e28fc35517f7a46a0952
leonardopppo <95853744+leonardopppo@users.noreply.github.com>
2026-09-04 17:59:36 +0200

    creazione file iniziale

---
+++ congressoMedico/src/main/java/com/factoryStudios/gestione/congresso/CongressoApplication.java
@@ -0,0 +1,13 @@
+package com.factoryStudios.gestione.congresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongressoApplication.class, args);
+	}
+
+}
---
+++ congressoMedico/src/test/java/com/factoryStudios/gestione/congresso/CongressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoryStudios.gestione.congresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
